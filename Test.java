62094cfb3bc92c11b3ba0f7338085f9e7d5ecf37
MarkMainComp <mark.seatang@hotmail.com>
2017-01-07 22:30:11 +1100

    Serves non-selectable marvel comic id

--- Test.java
+++ Test.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.lang.StringBuilder;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -34,12 +40,30 @@ public class Test {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
+           System.out.println("My path was: " + this.getPath());
+//            String response = "http://gateway.marvel.com/v1/public/comics/2?ts=1&apikey=46ec917e23a4130cf16fd110c47f0e8e&hash=e3b9d91f70a83e60f3d91bf2230b84ca";
+            String response = getHTML("http://gateway.marvel.com/v1/public/comics/2?ts=1&apikey=46ec917e23a4130cf16fd110c47f0e8e&hash=e3b9d91f70a83e60f3d91bf2230b84ca");
+            t.sendResponseHeaders(200, 0); // 2nd arg length set to 0 to allow arbitrary length resp.
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
         }
+    }
+
+    // Code sourced from stack exchange :p
+    // http://stackoverflow.com/a/1485730
+    public static String getHTML(String urlToRead) throws IOException {
+       StringBuilder result = new StringBuilder();
+       URL url = new URL(urlToRead);
+       HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+       conn.setRequestMethod("GET");
+       BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+       String line;
+       while ((line = rd.readLine()) != null) {
+          result.append(line);
+       }
+       rd.close();
+       return result.toString();
     }
 
 }
