5511028ff166e9825657380afbb92638f5cae5f9
MarkMainComp <mark.seatang@hotmail.com>
2017-01-08 19:34:35 +1100

    Fixed formatting of outputs - outputs proper json now

--- MarvelHandler.java
+++ MarvelHandler.java
@@ -44,6 +44,7 @@ public class MarvelHandler implements HttpHandler {
          Integer id = Integer.valueOf(arg);
          comicIdQueue.add(id);
       }
+      int numIds = comicIdQueue.size();
 
       // Create my threads to do the requests
       int numCores = Runtime.getRuntime().availableProcessors();
@@ -63,13 +64,45 @@ public class MarvelHandler implements HttpHandler {
       } catch (InterruptedException e) {
          e.printStackTrace();
       }
-
+      // After threads have joined, we'll know how many responses we got
+      int numResponses = responseQueue.size();
 
       t.sendResponseHeaders(200, 0); // 2nd arg length set to 0 to allow arbitrary length resp.
       OutputStream os = t.getResponseBody();
-      for (String response : responseQueue) {
-         os.write(response.getBytes());
+      String head = header(numIds, numResponses);  
+
+      String tail = footer();
+      os.write(head.getBytes()); // Write header
+      int i = 0;
+      for (String currOutput : responseQueue) {
+         currOutput = strip(currOutput);
+         
+         if (i != responseQueue.size() - 1) {
+            currOutput = currOutput.concat(",");
+         }
+         os.write(currOutput.getBytes());
+         i++;
       }
+      os.write(tail.getBytes());
       os.close();
    }
+
+   // This lets us generate something that looks like the header of the json file
+   private String header (int total, int count) {
+      return "{\"code\":200,\"status\":\"Ok\",\"copyright\":\" 2017 MARVEL\",\"attributionText\":\"Data provided by Marvel.  2017 MARVEL\",\"attributionHTML\":\"<a href=\\\"http://marvel.com\\\">Data provided by Marvel.  2017 MARVEL</a>\",\"etag\":\"9595ee96720036501b9bfc79c13824bed3ac96f7\",\"data\":{\"offset\":0,\"limit\":20,\"total\":" + total + ",\"count\":" + count + ",\"results\":[";
+   }
+   // This lets us generate something that looks like the footer of the json file
+   private String footer () {
+      return "]}}"; 
+   }
+
+   // Given a raw json input from the marvel API, strip out only the results section and return it
+   private String strip (String input) {
+
+      String[] header = input.split("results\"\\:\\["); // because i only want all the chars after results:[
+      String tail = header[1];
+      String output = tail.substring(0, tail.length() - 3); // strip off the trailing "]}}" sequence*/
+      return output;
+   }
+
 }
